ff44f8873a4fcb926bb7b01d93000e182de25370
Bohdan99I <bohdan.voiyov@gmail.com>
2023-07-08 21:41:19 +0300

    creat-js

---
+++ src/js/support-img.js
@@ -0,0 +1,37 @@
+export const imegesSupport = [{
+    title: 'Save the Children',
+    img: new URL(`../images/support-1.png`, import.meta.url), 
+},
+{
+    title: 'Project HOPE',
+    img: new URL(`../images/support-2.png`, import.meta.url),   
+},
+{
+    title: 'UNITED24',
+    img: new URL(`../images/support-3.png`, import.meta.url), 
+},
+{
+    title: 'International Medical Corps',    
+    img: new URL(`../images/support-4.png`, import.meta.url),  
+},
+{
+    title: 'Medicins Sans Frontieres',   
+    img: new URL(`../images/support-5.png`, import.meta.url),
+},
+{
+    title: 'RAZOM',  
+    img: new URL(`../images/support-6.png`, import.meta.url),
+},
+{
+    title: 'Action against hunger',   
+    img: new URL(`../images/support-7.png`, import.meta.url),
+},
+{
+    title: 'World vision',   
+    img: new URL(`../images/support-8.png`, import.meta.url), 
+},
+{
+    title: 'Serhiy Prytula Charity Foundation',  
+    img: new URL(`../images/support-9.png`, import.meta.url), 
+},
+];
---
+++ src/js/support-items.js
@@ -0,0 +1,39 @@
+export const supportItems = [{
+    title: 'Save the Children',
+    url:
+      'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',    
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',    
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',    
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',    
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',    
+  },
+];
