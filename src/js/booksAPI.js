1e5c1fa978d957b351b079a61feb690527352a87
SergrPashkov <SGpashkovsg@gmail.com>
2023-07-08 21:32:15 +0300

    Update booksAPI.js

--- src/js/booksAPI.js
+++ src/js/booksAPI.js
@@ -3,32 +3,43 @@
 // https://books-backend.p.goit.global/books/top-books
 // https://books-backend.p.goit.global/books/category?category=selectedCategory
 // https://books-backend.p.goit.global/books/ bookId
+import axios from "axios";
 
+export class booksAPI {
+	#BASE_URL = 'https://books-backend.p.goit.global';
 
+async getBookByCategoryLists() {
+	const resp = await axios.get(`${this.#BASE_URL}/books/category-list`)
+	if (!resp.statusText === 'OK') {
+		throw new Error('Not found')
+	}
+	return resp;
+};
 
+async getBookByCategory(categoryList) {
+	console.log(categoryList);
+	const resp = await axios.get(`${this.#BASE_URL}/books/${categoryList}`)
+	if (!resp.statusText === 'OK') {
+		throw new Error('Not found')
+	}
+	return resp;
+};
 
-// import axios from "axios";
+async getBestSellers(topBooks) {
+      console.log(topBooks);
+		const resp = await axios.get(`${this.#BASE_URL}/books/${topBooks}`)
+			if (!resp.statusText === 'OK') {
+				throw new Error('Not found')
+			}
+		return resp;
+}
 
-// export class booksAPI {
-//   #BASE_URL = '';
-
-//   constructor() {
-//   }
-
-//   getBestSellers(page) {
-//     return axios.get(`${this.#BASE_URL}/......`, {
-//       params: {
-
-//       }
-//     })
-//   }
-
-//   getBookByCategory(page) {
-//     return axios.get(`${this.#BASE_URL}/......`, {
-//       params: {
-
-//       }
-//     })
-//   }
-
-// }
+async getBookById(bookId) {
+   console.log(bookId);
+	const resp = await axios.get(`${this.#BASE_URL}/books/${bookId}`)
+	if (!resp.statusText === 'OK') {
+		throw new Error('Not found')
+	}
+	return resp;
+}
+};
