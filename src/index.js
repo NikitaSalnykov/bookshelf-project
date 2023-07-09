2444425bb34b1be5460e8819cad283e91b2d8176
TaniaMateichuk <tania.0309.tsyra@gmail.com>
2023-07-09 12:40:09 +0300

    mobile-menu-authorization-modals/Tania

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './js/bestSellers.js';
 
 import './js/shopping-list/shoppingList';
+import './js/mobile-menu.js';
---
+++ src/js/mobile-menu.js
@@ -0,0 +1,28 @@
+// // -------- кнопка відкривання
+// const btnOpen = document.querySelector('.js-open-menu'); 
+// // -------- кнопка закривання
+// const btnClose = document.querySelector('.js-close-menu');
+// const menuContainer = document.querySelector('.js-menu-container');
+
+// btnOpen.addEventListener('click', onClickBtnOpen);
+// btnClose.addEventListener('click', onClickBtnClose);
+
+// let activdisplay = getComputedStyle(btnClose).display;
+// function onClickBtnOpen() {
+//     btnOpen.classList.add('is-open');
+//     menuContainer.classList.add('is-open');
+//     if (btnOpen.classList.contains('is-open')) {
+//         btnClose.classList.add('is-open');
+//         btnClose.classList.toggle('block');
+//         btnOpen.style.display = 'none';
+//     }
+// }
+
+// function onClickBtnClose() {
+//     btnOpen.classList.remove('is-open');
+//     btnClose.classList.toggle('block');
+//     btnClose.classList.remove('is-open');
+//     menuContainer.classList.remove('is-open');
+//     btnOpen.style.display = 'block';
+//   // btnOpen.classList.toggle('block');
+// }
