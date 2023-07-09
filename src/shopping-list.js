42014530a22644954bdea5c7a1271e354e182aaa
Roman Gritsik <111363152+Binbinik@users.noreply.github.com>
2023-07-09 17:46:46 +0300

    support to shopping

---
+++ src/shopping-list.js
@@ -0,0 +1,13 @@
+import svgBin from './images/sprite.svg';
+import emptyDtTab1x from './images/shop-list/empty-desktop-tablet@1x.png';
+import emptyDtTab2x from './images/shop-list/empty-desktop-tablet@2x.png';
+import emptyMob1x from './images/shop-list/empty-mobile@1x.png';
+import emptyMob2x from './images/shop-list/empty-mobile@2x.png';
+import amazonPng from './images/png-icons/shops/amazon-icon1x.png';
+import amazonPng2x from './images/png-icons/shops/amazon-icon2x.png';
+import appleBookPng from './images/png-icons/shops/applebook-icon1x.png';
+import appleBookPng2x from './images/png-icons/shops/applebook-icon2x.png';
+import bookShopPng from './images/png-icons/shops/bookshop-icon1x.png';
+import bookShopPng2x from './images/png-icons/shops/bookshop-icon2x.png';
+
+import Pagination from 'tui-pagination';
