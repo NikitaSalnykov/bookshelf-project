50654738fb1339da1ade3bf88fdfcd8acc999bc9
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-07-10 18:05:43 +0300

    1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './js/bestSellers.js';
 import './js/sidebarCategories.js';
 import './js/support.js'
 
-import './js/shopping-list/shoppingList';
 
 //import './js/firebase/firebase-test.js';
 
