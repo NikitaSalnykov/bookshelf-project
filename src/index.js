f2acc5ff520cec795e1c78578c278cecaf083a63
SergrPashkov <SGpashkovsg@gmail.com>
2023-07-13 17:38:55 +0300

    addFooter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import './js/checkAuth.js';
-import './js/markup.js';
-
-import './js/sidebarCategories.js';
-import './js/support.js'
-
-// import './js/firebase/firebase-test.js';
+import './js/bestSellers.js';
 
 import './js/theme.js';
-import './js/sign.js';
-import './js/LogOut.js';
-import './js/API.js';
-import './js/modal.js';
-import './js/scrollup.js';
-import './js/mobil-menu.js'
-import './js/switcherActive'
 
+import './js/support.js'
+
+import './js/shopping-list/shoppingList';
+
+//import './js/firebase/firebase-test.js';
+
+import './js/sign.js';
+
+import './js/allCategories.js';
